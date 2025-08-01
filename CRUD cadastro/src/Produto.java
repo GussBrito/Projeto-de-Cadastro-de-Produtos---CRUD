1edd8394769c52be6c9518b5f9ad475633a63610
Gustavo.B <britogustavo88@gmail.com>
2025-08-01 09:44:01 -0300

    Add código:Produto.java

--- CRUD cadastro/src/Produto.java
+++ CRUD cadastro/src/Produto.java
@@ -1,2 +1,65 @@
-public class Produto {
+import java.io.Serializable;
+import java.lang.annotation.Documented;
+import java.util.Objects;
+
+public class Produto implements Serializable {
+    // Indentificador único para a versão da classe durante a serialização.
+    private static final long seralVersionUID = 1L;
+
+    private int codigo;
+    private String descricao;
+    private double preco;
+
+    public Produto(int codigo, String descricao, double preco){
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.codigo = codigo;
+    }
+    //Getters e Setters
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    // Sobrescrever equals() e hashCode() para tratar um produto como único pelo código.
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(codigo);
+    }
+    @Override
+    public String toString(){
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", descricao+'" + descricao + '\'' +
+                ", preco=" +preco+
+                '}';
+    }
 }
